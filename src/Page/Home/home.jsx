5597c90962ad51e231dc53d2e0ff0c1e28a29546
Shakhmurad <muradb356@gmail.com>
2024-07-11 01:48:15 +0400

    add box

--- src/Page/Home/home.jsx
+++ src/Page/Home/home.jsx
@@ -5,6 +5,7 @@ function Home() {
   return (
     <>
     <PageContainer>
+      {/* ----Nav---- */}
         <div className="nav-container">
           <div className="nav-text">
             <h2>Create a website without limits</h2>
@@ -17,7 +18,32 @@ function Home() {
           <div className="nav-img">
             <img src="./Screenshot_1.png" />
           </div>
+        </div>
 
+
+        {/* ----Box-text---- */}
+        <div className="box-center">
+        <div className="box-container">
+            <h2>One platform,infinite possibilities</h2>
+            <div className="box-container-block">
+              <div className="box-block">
+                  <h3>Build a website</h3>
+                  <p>Design with a full suite of intuitive tools and powerful AI to create the site you want.</p>
+              </div>
+              <div className="box-block">
+                  <h3>Manage your business
+                  </h3>
+                  <p>Design with a full suite of intuitive tools and powerful AI to create the site you want.</p>
+              </div>
+              <div className="box-block">
+                  <h3>Grow online</h3>
+                  <p>Design with a full suite of intuitive tools and powerful AI to create the site you want.</p>
+              </div>
+            </div>
+            <div className="box-button">
+                <button>Get Started</button>
+            </div>
+        </div>
         </div>
     </PageContainer>
     </>
